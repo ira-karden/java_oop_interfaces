070ca6e99ae18a2205e4a1e14137fe49a3fc3799
Irina Karden <karden@macbook-pro-karden.local>
2022-02-14 23:31:57 +0200

    delete metods set/get from interfaces

--- com.example.java/src/com/company/details/IEngine.java
+++ com.example.java/src/com/company/details/IEngine.java
@@ -2,10 +2,6 @@ package com.company.details;
 
 public interface IEngine {
 
-    public void setProducer(String producer);
-    public String getProducer();
-    public void setPower(double power);
-    public double getPower();
     public String toString();
     public void start();
     public void stop();
--- com.example.java/src/com/company/vehicles/ICar.java
+++ com.example.java/src/com/company/vehicles/ICar.java
@@ -4,15 +4,6 @@ import com.company.details.Engine;
 import com.company.professions.Driver;
 
 public interface ICar {
-    public String getBrand();
-    public void setBrand(String brand);
-    public String getType();
-    public void setType(String type);
-    public double getWeight();
-    public void setWeight(double weight);
-    public Engine getEngine();
-    public Driver getDriver();
-    public void setDriver(Driver driver);
     public void start();
     public void stop();
     public void turnRight();
